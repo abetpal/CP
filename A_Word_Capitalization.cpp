e901a7b2d92100edb5569a4ffe038948f8916a3a
abetpal <patelabhinav61@gmail.com>
2021-11-04 22:28:58 +0530

    CP

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,20 @@
+//author: nobrakebicycle
+//created: 04.11.2021 22:17:34
+//Hello Traveller
+//आज रपट जायें तो हमें ना कहियो
+//कल फिसल जायें तो हमें ना उठइयो
+//Time and Space wait for none.(especially in programming, its just dilemma) ◉‿◉
+//Dher Mithai mein keeya padela
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s;
+    return 0;
+}
