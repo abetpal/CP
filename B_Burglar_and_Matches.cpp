1344f702a720b3f1f1566d702de3d598021a0597
abetpal <patelabhinav61@gmail.com>
2021-11-09 21:53:02 +0530

    CP

--- B_Burglar_and_Matches.cpp
+++ B_Burglar_and_Matches.cpp
@@ -9,17 +9,37 @@
 #define ll long long
 #define lli long long int
 using namespace std;
+bool sortcol(const vector<ll int> &v1,
+             const vector<ll int> &v2)
+{
+    return v1[1] < v2[1];
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, m;
-    cin >> n, m;
-    vector<vector<int>> a;
-    for (int i = 0; i < m; i++)
+    ll int n, m;
+    cin >> n >> m;
+    vector<vector<ll int>> v(m, vector<ll int>(2));
+    for (ll int i = 0; i < m; i++)
     {
-        int a, b;
-        cin >> a >> b;
+        cin >> v[i][0] >> v[i][1];
     }
+    sort(v.rbegin(), v.rend(), sortcol);
+    ll int sum = v[0][0] * v[0][1];
+    ll int s = v[0][0];
+    m--;
+    ll int i = 1;
+    while (s < n && m)
+    {
+        sum += v[i][0] * v[i][1];
+        s += v[i][0];
+        i++;
+        m--;
+    }
+    //cout<<sum<<" "<<s<<" "<<i<<" "<<m<<"\n";
+    if (s > n)
+        sum -= (s - n) * v[i - 1][1];
+    cout << sum;
     return 0;
 }
