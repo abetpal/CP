93bead363735d14cfebe9eb6d75c5b2f7e992c86
abetpal <patelabhinav61@gmail.com>
2021-11-09 18:38:51 +0530

    CP

---
+++ B_Caisa_and_Pylons.cpp
@@ -0,0 +1,24 @@
+//author: nobrakebicycle
+//created: 09.11.2021 18:04:31
+//Hello Traveller
+//आज रपट जायें तो हमें ना कहियो
+//कल फिसल जायें तो हमें ना उठइयो
+//Time and Space wait for none.(especially in programming, its just dilemma) ◉‿◉
+//Dher Mithai mein keeya padela
+#include <bits/stdc++.h>
+#define ll long long
+#define lli long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a, a + n);
+    cout << a[n - 1] << endl;
+    return 0;
+}
