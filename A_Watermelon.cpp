a4bcac283d9ba0ad8ed945cf6f35febb9d210bd6
abetpal <patelabhinav61@gmail.com>
2021-11-30 22:09:42 +0530

    CP

---
+++ A_Watermelon.cpp
@@ -0,0 +1,27 @@
+//author: nobrakebicycle
+//created: 30.11.2021 22:01:26
+//Hello Traveller
+//आज रपट जायें तो हमें ना कहियो
+//कल फिसल जायें तो हमें ना उठइयो
+//Time and Space wait for none.(especially in programming, its just dilemma) ◉‿◉
+//Dher Mithai mein keeya padela
+#include <bits/stdc++.h>
+#define ll long long
+#define lli long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    if (t % 2 != 0 || t == 2)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << "YES";
+    }
+    return 0;
+}
