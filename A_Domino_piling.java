292710ebb0fc55a3ee9164043a65948ef99d9c38
abetpal <patelabhinav61@gmail.com>
2021-12-06 19:48:20 +0530

    CP

---
+++ A_Domino_piling.java
@@ -0,0 +1,45 @@
+//author: nobrakebicycle
+//created: 06.12.2021 19:45:41 
+//Hello Traveller
+//आज रपट जायें तो हमें ना कहियो
+//कल फिसल जायें तो हमें ना उठइयो
+//Time and Space wait for none.(especially in programming, its just dilemma) ◉‿◉
+//Dher Mithai mein keeya padela
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+public class A_Domino_piling {
+    public static void main(String args[]) throws Exception {
+        /*
+         * Sample code to perform I/O:
+         * Use either of these methods for input
+         * 
+         * //BufferedReader
+         * BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         * String name = br.readLine(); // Reading input from STDIN
+         * System.out.println("Hi, " + name + "."); // Writing output to STDOUT
+         * 
+         * //Scanner
+         * Scanner s = new Scanner(System.in);
+         * String name = s.nextLine(); // Reading input from STDIN
+         * System.out.println("Hi, " + name + "."); // Writing output to STDOUT
+         * 
+         */
+
+        // Write your code here
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int d = n * m / 2;
+        System.out.println(d);
+
+    }
+}
