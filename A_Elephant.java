ac3c7fd5ef8f898aae4c666cc42357b33198343f
abetpal <patelabhinav61@gmail.com>
2021-12-08 16:40:44 +0530

    CP

---
+++ A_Elephant.java
@@ -0,0 +1,43 @@
+//author: nobrakebicycle
+//created: 08.12.2021 16:39:07 
+//Hello Traveller
+//आज रपट जायें तो हमें ना कहियो
+//कल फिसल जायें तो हमें ना उठइयो
+//Time and Space wait for none.(especially in programming, its just dilemma) ◉‿◉
+//Dher Mithai mein keeya padela
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+public class A_Elephant {
+    public static void main(String args[]) throws Exception {
+        /*
+         * Sample code to perform I/O:
+         * Use either of these methods for input
+         * 
+         * //BufferedReader
+         * BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         * String name = br.readLine(); // Reading input from STDIN
+         * System.out.println("Hi, " + name + "."); // Writing output to STDOUT
+         * 
+         * //Scanner
+         * Scanner s = new Scanner(System.in);
+         * String name = s.nextLine(); // Reading input from STDIN
+         * System.out.println("Hi, " + name + "."); // Writing output to STDOUT
+         * 
+         */
+
+        // Write your code here
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println((x + 4) / 5);
+
+    }
+}
