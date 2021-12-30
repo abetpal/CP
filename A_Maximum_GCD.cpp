6b3e76955757bea24bfaaf776a3cec36daa01d17
abetpal <patelabhinav61@gmail.com>
2021-12-30 14:10:13 +0530

    CP

---
+++ A_Maximum_GCD.cpp
@@ -0,0 +1,25 @@
+//author: nobrakebicycle
+//created: 30.12.2021 14:09:53
+//Hello Traveller
+//आज रपट जायें तो हमें ना कहियो
+//कल फिसल जायें तो हमें ना उठइयो
+//Time and Space wait for none.(especially in programming, its just dilemma) ◉‿◉
+//Dher Mithai mein keeya padela
+#include <bits/stdc++.h>
+#define ll long long
+#define lli long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        cout << n / 2 << endl;
+    }
+    return 0;
+}
