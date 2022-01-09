1cddd0389a567336469a89ebb2b1d099e43c5b2b
abetpal <patelabhinav61@gmail.com>
2022-01-09 17:11:02 +0530

    CP

---
+++ Big_Sorting.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bigSorting' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts STRING_ARRAY unsorted as parameter.
+     */
+
+    public static List<String> bigSorting(List<String> unsorted) {
+    // Write your code here
+     String[] array = unsorted.toArray(new String[unsorted.size()]);  
+     Arrays.sort(array, (left, right) ->
+        {
+            if (left.length() != right.length())
+                return left.length() - right.length();
+             return left.compareTo(right);
+        });  
+        List<String> sorted=new ArrayList<String>();  
+    for(String lang:array){  
+    sorted.add(lang);  
+}
+return sorted;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> unsorted = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<String> result = Result.bigSorting(unsorted);
+
+        bufferedWriter.write(
+            result.stream()
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
