795b942e70679a657f8e7ac1a2ce9a54f1b08922
abetpal <patelabhinav61@gmail.com>
2022-01-13 17:41:58 +0530

    CP

---
+++ A_Special_Permutation.cpp
@@ -0,0 +1,29 @@
+//author: nobrakebicycle
+//created: 13.01.2022 17:41:32
+//Hello Traveller
+//आज रपट जायें तो हमें ना कहियो
+//कल फिसल जायें तो हमें ना उठइयो
+//Time and Space wait for none.(especially in programming, its just dilemma) ◉‿◉
+//Dher Mithai mein keeya padela
+#include <bits/stdc++.h>
+#define ll long long
+#define lli long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; ++i)
+        {
+            cout << (i + 1) % n + 1 << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
