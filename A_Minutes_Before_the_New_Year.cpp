72ef9fc9ba014fdfee5ec6694a14e52349626a73
abetpal <patelabhinav61@gmail.com>
2022-01-15 19:58:45 +0530

    CP

--- A_Minutes_Before_the_New_Year.cpp
+++ A_Minutes_Before_the_New_Year.cpp
@@ -13,11 +13,10 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int T;
-    cin >> T;
-    while (T--)
+    int q;
+    scanf("%d", &q);
+    for (int i = 0; i < q; ++i)
     {
-
         int h, m;
         scanf("%d %d", &h, &m);
         printf("%d\n", 1440 - h * 60 - m);
