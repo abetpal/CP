5d039373a9093c5d8c8b14e2b873c7e3794c893e
abetpal <patelabhinav61@gmail.com>
2022-01-15 19:57:40 +0530

    CP

---
+++ A_Minutes_Before_the_New_Year.cpp
@@ -0,0 +1,26 @@
+//author: nobrakebicycle
+//created: 15.01.2022 19:56:56
+//Hello Traveller
+//आज रपट जायें तो हमें ना कहियो
+//कल फिसल जायें तो हमें ना उठइयो
+//Time and Space wait for none.(especially in programming, its just dilemma) ◉‿◉
+//Dher Mithai mein keeya padela
+#include <bits/stdc++.h>
+#define ll long long
+#define lli long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+
+        int h, m;
+        scanf("%d %d", &h, &m);
+        printf("%d\n", 1440 - h * 60 - m);
+    }
+    return 0;
+}
